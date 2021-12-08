d0f9058f38ca4cf6471bf4bf430aecd50443a025
jihyunlee <jihyunlee@postech.ac.kr>
2021-12-09 01:43:41 +0900

    add test in collacate

--- dataset.py
+++ dataset.py
@@ -188,7 +188,7 @@ class Dataset(torch.utils.data.Dataset):
         dial_id = [x["dial_id"] for x in batch]
         turn_id = [x["turn_id"] for x in batch]
         
-        if do_student:
+        if do_student or self.type == 'test':
             texts = [self.tokenizer.decode(x["source"]["input_ids"]) for x in batch]
             idxs = [t.rfind('belief:') for t in texts] # find from behind
             prior_texts = [t[:idx] for (t,idx) in zip(texts, idxs)]
